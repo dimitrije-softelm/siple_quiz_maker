43dfd4d7b32432310d94fc0c74395974c6ab3103
dimitrije-softelm <dimitrije@softelm.com>
2022-09-28 14:54:11 +0200

    feature: added router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
+import QuizzesPage from './Pages/Quizzes/QuizzesPage';
+import QuestionsPage from './Pages/Questions/QuestionsPage';
 
 function App() {
   return (
     <div className="App">
-
+    <Routes>
+      <Route path="questions" element={<QuestionsPage />} />
+      <Route path="quizzes" element={<QuizzesPage />} />
+    </Routes>
     </div>
   );
 }
---
+++ src/Pages/Questions/QuestionsPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const QuestionsPage = () => {
+  return (
+    <div></div>
+  );
+}
+
+export default QuestionsPage;
